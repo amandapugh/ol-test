295d194ede0df377af0fcce0847eebb6c6c360ab
amandapugh <awoolvert@gmail.com>
2024-07-16 13:46:20 -0400

    Added Open Layers

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <ol-map
+    :loadTilesWhileAnimating="true"
+    :loadTilesWhileInteracting="true"
+    style="height: 400px"
+  >
+    <ol-view
+      ref="view"
+      :center="center"
+      :rotation="rotation"
+      :zoom="zoom"
+      :projection="projection"
+    />
+      <ol-tile-layer>
+        <ol-source-osm />
+      </ol-tile-layer>
+    </ol-map>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, inject } from 'vue'
+  
+  const center = ref([40, 40])
+  const projection = ref('EPSG:4326')
+  const zoom = ref(3)
+  const rotation = ref(0)
+  
+  const format = inject('ol-format')
+  const geoJson = new format.GeoJSON()
+  console.log(geoJson)
+  </script>
+  
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import "vue3-openlayers/styles.css"
+import OpenLayersMap from "vue3-openlayers"
+
+createApp(App).use(OpenLayersMap).mount('#app')
